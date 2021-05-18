0fa61163ce1701e44dcfaede9dc09b0d0b27dabd
Jonas Almlöf <>
2021-05-18 08:37:01 +0200

    Added hotspot variant info to QC

--- src/Snakemake/rules/QC/check_coverage.smk
+++ src/Snakemake/rules/QC/check_coverage.smk
@@ -3,10 +3,11 @@
 rule check_DNA_coverage:
     input:
         bam = "DNA_bam/{sample}-ready.bam",
-        bai = "DNA_bam/{sample}-ready.bam.bai"
+        bai = "DNA_bam/{sample}-ready.bam.bai",
+        vcf = "Results/DNA/{sample}/vcf/{sample}-ensemble.final.no.introns.AD20.vep.multiplebp.vcf",
     output:
         coverage = "Results/DNA/{sample}/QC/Low_coverage_positions.txt",
-        coverage2 = "Results/DNA/{sample}/QC/All_coverage_positions.txt"
+        coverage2 = "Results/DNA/{sample}/QC/All_coverage_positions.txt",
     run:
         import subprocess
-        subprocess.call("python src/check_coverage.py " + input.bam + " " + output.coverage + " " + output.coverage2, shell=True)
+        subprocess.call("python src/check_coverage.py " + input.bam + " " + input.vcf + " " + output.coverage + " " + output.coverage2, shell=True)
--- src/check_coverage.py
+++ src/check_coverage.py
@@ -8,12 +8,14 @@ path = "/data/ref_data/wp1/refFiles_20191010/refFiles/"
 infiles = ["Mutations_Colon_20171219.csv", "Mutations_Lung_20190211.csv", "Mutations_Melanom_20190211.csv", "Mutations_Gist_20190211.csv","Mutations_Ovarial_20170125_header_ok.csv"]
 
 bam_file = sys.argv[1]
-outfile = open(sys.argv[2], "w")
-outfile2 = open(sys.argv[3], "w")
+vcf_file = sys.argv[2]
+outfile = open(sys.argv[3], "w")
+outfile2 = open(sys.argv[4], "w")
 
 
 outfile.write("#Chr\tStart_hg19\tEnd_hg19\tGene\tCDS_mut_syntax\tAA_mut_synta\tReport\tcomment\tExon\tAccession_number\tCoverage\tPosition\n")
-outfile2.write("#Chr\tStart_hg19\tEnd_hg19\tGene\tCDS_mut_syntax\tAA_mut_synta\tReport\tcomment\tExon\tAccession_number\tCoverage\tPosition\n")
+outfile2.write("#Chr\tStart_hg19\tEnd_hg19\tGene\tCDS_mut_syntax\tAA_mut_synta\tReport\tcomment\tExon\tAccession_number\tCoverage\tPosition")
+outfile2.write("\tDP\tRef_DP\tAlt_DP\tAF\n"
 
 
 '''Find positions to report and gene regions to analyse'''
@@ -70,6 +72,57 @@ for gene_region in gene_regions :
         gene_regions_temp.append(gene_region)
 gene_regions = gene_regions_temp
 
+
+'''find all calls in the vcf overlapping hotspots'''
+vcf_dict = {}
+header = True
+for line in vcf:
+    if header:
+        if line[:6] == "#CHROM":
+            header = False
+        continue
+    lline = line.strip().split("\t")
+    chrom = lline[0][3:]
+    pos = lline[1]
+    key = chrom + "_" + pos
+    INFO = lline[7].split(";")
+    FORMAT = lline[8].split(":")
+    DATA = lline[9].split(":")
+    if INFO[:3] == "AA=":
+        continue
+    AD_index = 0
+    DP_index = 0
+    RD_index = 0
+    i = 0
+    for f in FORMAT:
+        if f == "AD":
+            AD_index = i
+        if f == "DP":
+            DP_index = i
+        if f == "RD":
+            RD_index = i
+        i += 1
+    AD = DATA[AD_index].split(",")
+    Ref_DP = 0
+    Alt_DP = 0
+    if len(AD) == 2:
+        Ref_DP = AD[0]
+        Alt_DP = AD[1]
+    else:
+        Ref_DP = DATA[RD_index]
+        Alt_DP = DATA[AD_index]
+    DP = DATA[DP_index]
+    AF_index = 0
+    i = 0
+    for info in INFO:
+        if info[:3] == "AF=":
+            AF_index = i
+        i += 1
+    AF = INFO[AF_index][3:]
+    if key in inv_pos:
+        vcf_dict[key] = [DP, Ref_DP, Alt_DP, AF]
+
+
 '''Report all interesting positions (All but region) with coverage < 50'''
 depth_dict = {}
 for region in gene_regions :
@@ -93,7 +146,11 @@ for region in gene_regions :
                 outfile.write(str(coverage) + "\t" + pos + "\n")
             for info in inv_pos[key] :
                 outfile2.write(info + "\t")
-            outfile2.write(str(coverage) + "\t" + pos + "\n")
+            outfile2.write(str(coverage) + "\t" + pos)
+            if key in vcf_dict:
+                for info in vcf_dict[key]:
+                    outfile2.write("\t" + str(info))
+            outfile2.write("\n")
     depth_file.close()
 outfile.close()
 outfile2.close()
